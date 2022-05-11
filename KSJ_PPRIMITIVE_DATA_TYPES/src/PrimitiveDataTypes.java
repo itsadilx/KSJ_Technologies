61dbca452798d3ee0c679cc316d0a1edb081fc01
Adil Shaikh <adilshaikhhh@outlook.com>
2022-05-11 21:00:12 +0530

    Primitive Data Types

---
+++ KSJ_PPRIMITIVE_DATA_TYPES/src/PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) 
+	{
+		char a='S';
+		byte b=3;
+		short c=33;
+		int d=45;
+		float e=5.12345f;
+		double f=4.682952335335d;
+		boolean g = true ;
+		long h= 10000 ;
+		
+		System.out.println("char:"+a);
+		System.out.println("byte:"+b);
+		System.out.println("short:"+c);
+		System.out.println("int:"+d);
+		System.out.println("float:"+e);
+		System.out.println("double:"+f);
+		System.out.println("boolean:"+g);
+		System.out.println("long:"+h);
+
+	}
+
+}
