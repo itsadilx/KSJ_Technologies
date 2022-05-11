a1d9308d75e15addb9b3699fd7436aa54c0cd657
Adil Shaikh <adilshaikhhh@outlook.com>
2022-05-11 20:59:18 +0530

    DATA CONVERSION

---
+++ KSJ_DT_CONVERSION/src/ExplicitDataTypeConversion.java
@@ -0,0 +1,21 @@
+
+public class ExplicitDataTypeConversion {
+
+	public static void main(String[] args) 
+	{
+		double a=50.50;
+	    System.out.println("double reprresentation:"+a);
+	    
+	    float f=(float)a;
+	    System.out.println("float reprresentation:"+f);
+
+	    long b=(long)a;
+	    System.out.println("long reprresentation:"+b);
+         
+	    int c=(int)b;
+	    System.out.println("int reprresentation:"+c);
+
+
+	}
+
+}
---
+++ KSJ_DT_CONVERSION/src/ImplicitDataTypeConversion.java
@@ -0,0 +1,18 @@
+
+public class ImplicitDataTypeConversion {
+
+	public static void main(String[] args)
+	{
+		int a=555;
+	    System.out.println("Int reprresentation:"+a);
+	    
+	    long b=a;
+	    System.out.println("long reprresentation:"+b);
+
+	    float c=b;
+	    System.out.println("float reprresentation:"+c);
+
+
+	}
+
+}
